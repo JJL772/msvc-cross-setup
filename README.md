82377d8ac7d3784ffdb1de508375e443ad2c3202
JJL772 <jeremy.lorelli.1337@gmail.com>
2023-01-16 15:45:00 -0800

    update

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ A tiny little wrapper around msvc-wine
 ./setup-lldb.sh to setup lldb within your current `WINEPREFIX`, for easier debugging.
 
 ./setup.sh to setup stuff needed to build Source engine tools/games.
+
+## Usage
+
+Make sure to clone recursively
+
+```sh
+# Setup the base MSVC stuff, incl. MFC/ATL
+./setup.sh ~/msvc
+
+# Setup LLDB for easier debugging
+./setup-lldb.sh ~/msvc
+
+# Fixup bad includes
+./fix-inc.sh ~/msvc
+```
--- fix-inc.sh
+++ fix-inc.sh
@@ -7,8 +7,18 @@ if [[ -z "$1" ]]; then
 	exit 1
 fi
 
+cd "$(dirname "$0")"
+
+# Fixup kits
 for kit in "$1/kits/10/Include/"*; do
-	ln -svf "$kit/um/ole2.h" "$kit/um/Ole2.h"
-	ln -svf "$kit/um/olectl.h" "$kit/um/OleCtl.h"
+	#ln -svf "$kit/um/ole2.h" "$kit/um/Ole2.h"
+	#ln -svf "$kit/um/olectl.h" "$kit/um/OleCtl.h"
+	./msvc-wine/fixinclude "$kit/um/"
+done
+
+# Fixup MFC/ATL
+for msvc in "$1/VC/Tools/MSVC"*; do
+	./msvc-wine/lowercase "$msvc/atlmfc/include"
+	./msvc-wine/fixinclude "$msvc/atlmfc/include"
 done
 
